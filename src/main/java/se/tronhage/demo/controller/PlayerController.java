1d026855bad13749d2707369780698d29acdd86d
RobertTronhage <143345075+RobertTronhage@users.noreply.github.com>
2024-09-15 14:17:52 +0200

    changed so that constant will take the amount of games in consideration, if player has less than 5 games, the constant should be lower. will att game-history to player

--- src/main/java/se/tronhage/demo/controller/PlayerController.java
+++ src/main/java/se/tronhage/demo/controller/PlayerController.java
@@ -6,20 +6,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import se.tronhage.demo.entity.Player;
 import se.tronhage.demo.service.PlayerService;
+
 import java.util.List;
 
 @RestController
-@RequestMapping("/players") public class PlayerController {
+@RequestMapping("/players")
+public class PlayerController {
 
     @Autowired
     private PlayerService playerService;
 
     @GetMapping("/getallplayers")
-    public List<Player> getPlayerData(){
+    public List<Player> getPlayerData() {
         return playerService.getAllPlayers();
     }
+
     @PostMapping("/addplayer")
-    public String addPlayer(@RequestBody Player player){
+    public String addPlayer(@RequestBody Player player) {
         try {
             playerService.addNewPlayer(player.getName());
             return String.valueOf(new ResponseEntity<>("New player added", HttpStatus.CREATED));
--- src/main/java/se/tronhage/demo/utilities/Utilities.java
+++ src/main/java/se/tronhage/demo/utilities/Utilities.java
@@ -1,6 +1,6 @@
 package se.tronhage.demo.utilities;
 
-/*
+/**
 Author Robert Tronhage
 version 1.0
 Purpose of this file is to take in ELO from PlayerRepo and update the ELO based on result of match.
@@ -24,21 +24,34 @@ public class Utilities {
     @Autowired
     PlayerService playerService;
 
-    //if one player has played fewer than 5 matches, fix constant!
+    //if one player has played fewer than 5 matches, set constant to lower?
+
+    public int getConstant(Player player){
+        int constant;
+        int noOfGames = 0; // = player.getgameshistory.size;
+
+        if (noOfGames > 5){
+            constant = 30;
+        }else {
+            constant = 15;
+        }
+
+        return constant;
+    }
 
     public double[] calculateElo(Player player1, Player player2, Player winner) {
-        int constant = 30;
+        int constantP1 = getConstant(player1);
+        int constantP2 = getConstant(player2);
 
         double oddsOfWinningForPlayer1 = 1.0 / (1.0 + Math.pow(10, (player2.getElo() - player1.getElo()) / 400.0));
         double oddsOfWinningForPlayer2 = 1.0 / (1.0 + Math.pow(10, (player1.getElo() - player2.getElo()) / 400.0));
 
-        double calcElo1 = player1.getElo() + constant * ((player1.getId().equals(winner.getId()) ? 1 : 0) - oddsOfWinningForPlayer1);
-        double calcElo2 = player2.getElo() + constant * ((player2.getId().equals(winner.getId()) ? 1 : 0) - oddsOfWinningForPlayer2);
+        double calcElo1 = player1.getElo() + constantP1 * ((player1.getId().equals(winner.getId()) ? 1 : 0) - oddsOfWinningForPlayer1);
+        double calcElo2 = player2.getElo() + constantP2 * ((player2.getId().equals(winner.getId()) ? 1 : 0) - oddsOfWinningForPlayer2);
 
         double newElo1 = Math.round(calcElo1);
         double newElo2 = Math.round(calcElo2);
 
         return new double[]{newElo1, newElo2};
     }
-
 }
